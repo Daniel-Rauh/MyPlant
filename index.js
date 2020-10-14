bff97568c81e58d2d0435c77e97fe35cd038c760
Daniel-Rauh <daniel.rauh@gmail.com>
2020-10-14 09:45:39 +0200

    db setup

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cookieSession({
 
 app.use(express.static('public'))
 app.use(express.json())
-app.use(express.urlencoded( {extended: false}))
+app.use(express.urlencoded({ extended: false }))
 app.set('view engine', 'ejs')
 app.use(passport.initialize())
 app.use(passport.session())
@@ -51,4 +51,82 @@ app.get('/profile', (req, res) => {
 app.get('/logout', (req, res) => {
     req.logout()
     res.redirect('/')
+})
+
+app.get('/newRoom', (req, res) => {
+    res.status(200).render('newRoom')
+})
+
+app.post('/newRoom', (req, res) => {
+    User.find({ googleId: req.user.googleId })
+        .catch(err => console.log(err))
+        .then((result) => {
+            console.log(result[0]._id)
+            console.log(req.body)
+            if (result[0].rooms === undefined) {
+                const newRooms = []
+                newRooms.push(req.body)
+                User.findByIdAndUpdate(result[0]._id, { rooms: newRooms })
+                    .then((result) => {
+                        console.log(result)
+                        console.log('Rooms updated')
+                        res.status(201).redirect('/newRoom')
+                    })
+            } else {
+                const newRooms = result[0].rooms
+                newRooms.push(req.body)
+                User.findByIdAndUpdate(result[0]._id, { rooms: newRooms })
+                    .then((result) => {
+                        console.log(result)
+                        console.log('Rooms updated')
+                        res.status(201).redirect('/newRoom')
+                    })
+            }
+        })
+})
+
+app.get('/newPlant', (req, res) => {
+    res.status(200).render('newPlant')
+})
+
+app.post('/newPlant/:id', (req, res) => {
+    User.find({googleId: req.user.googleId})
+        .catch(err => console.log(err))
+        .then((result) => {
+            let resultRoom = ""
+            for (let i = 0; i < result[0].rooms.length; i++) {
+                if (result[0].rooms[i]._id == req.params.id) {
+                    resultRoom = result[0].rooms[i].name
+                }
+            }
+            if (result[0].plants === undefined) {
+                let newPlants = []
+                let newPlant = {
+                    room: resultRoom,
+                    name: req.body.name,
+                    species: req.body.species,
+                    picture: req.body.picture,
+                    schedule: req.body.schedule,
+                    needsWater: false
+                }
+                newPlants.push(newPlant)
+                User.findByIdAndUpdate(result._id, { plants: newPlants })
+                    .catch(err => console.log(err))
+                    .then(console.log('Plants updated'))
+            } else {
+                let newPlants = result[0].plants
+                let newPlant = {
+                    room: resultRoom,
+                    name: req.body.name,
+                    species: req.body.species,
+                    picture: req.body.picture,
+                    schedule: req.body.schedule,
+                    needsWater: false
+                }
+                newPlants.push(newPlant)
+                User.findByIdAndUpdate(result[0]._id, { plants: newPlants })
+                    .catch(err => console.log(err))
+                    .then(console.log('Plants updated'))
+            }
+        })
 })
--- models/user.js
+++ models/user.js
@@ -1,12 +1,27 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+const plantSchema = new Schema({
+    room: String,
+    name: String,
+    species: String,
+    picture: String,
+    schedule: Array,
+    needsWater: Boolean
+})
+
+const roomSchema = new Schema({
+    name: String,
+})
+
 const userSchema = new Schema({
     googleId: String,
     firstName: String,
     lastName: String,
     email: String,
-    picture: String
+    picture: String,
+    rooms: [roomSchema],
+    plants: [plantSchema]
 })
 
 const User = mongoose.model('user', userSchema)
