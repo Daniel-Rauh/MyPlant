00132cb1e0a67e573133811c4071be56b120997d
Daniel-Rauh <daniel.rauh@gmail.com>
2020-10-15 14:15:01 +0200

    v0.8

--- index.js
+++ index.js
@@ -41,7 +41,22 @@ app.get('/house', (req, res) => {
     User.find({ googleId: req.user.googleId })
         .catch(err => console.log(err))
         .then((result) => {
-            console.log(result[0].plants)
+            let totalPlants = 0
+            for (i = 0; i < result[0].plants.length; i++) {
+                totalPlants++
+            }
+            let newRooms = result[0].rooms
+            for (i = 0; i < result[0].rooms.length; i++) {
+                let room = result[0].rooms[i]
+                let plantCounter = 0
+                for (j = 0; j < result[0].plants.length; j++) {
+                    if (result[0].plants[j].room === result[0].rooms[i].name) {
+                        plantCounter++
+                    }
+                }
+                room.plants = plantCounter
+                newRooms[i] = room
+            }
             if (result[0].lastUpdated != day) {
                 let newPlants = result[0].plants
                 let newMiss = result[0].daysSinceMiss
@@ -66,6 +81,9 @@ app.get('/house', (req, res) => {
                     .catch(err => console.log(err))
                     .then(console.log("Watering needs updated"))
             }
+            User.findOneAndUpdate({ googleId: req.user.googleId }, { totalPlants: totalPlants, rooms: newRooms }, { useFindAndModify: false })
+                .catch(err => console.log(err))
+                .then(console.log("Numbers Updated"))
             res.status(200).render('house', { data: result[0], today: day })
         })
 })
@@ -88,10 +106,6 @@ app.get('/profile', (req, res) => {
 app.get('/logout', (req, res) => {
     req.logout()
     res.redirect('/')
-})
-
-app.get('/newRoom', (req, res) => {
-    res.status(200).render('newRoom')
 })
 
 app.get('/myHome', (req, res) => {
@@ -122,25 +136,43 @@ app.get('/room/:id', (req, res) => {
         })
 })
 
+app.get('/plant/:id', (req, res) => {
+    User.find({ googleId: req.user.googleId })
+        .catch(err => console.log(err))
+        .then((result) => {
+            let plant
+            for (i = 0; i < result[0].plants.length; i++){
+                if (result[0].plants[i]._id == req.params.id) {
+                    plant = result[0].plants[i]
+                }
+            }
+            res.status(200).render('plant', {plant: plant})
+        })
+})
+
 app.post('/newRoom', (req, res) => {
     User.find({ googleId: req.user.googleId })
         .catch(err => console.log(err))
         .then((result) => {
             if (result[0].rooms === undefined) {
                 const newRooms = []
-                newRooms.push(req.body)
-                User.findByIdAndUpdate(result[0]._id, { rooms: newRooms })
+                let newRoom = req.body
+                newRoom.plants = 0
+                newRooms.push(newRoom)
+                User.findByIdAndUpdate(result[0]._id, { rooms: newRooms }, {useFindAndModify: false})
                     .then((result) => {
                         console.log('Rooms updated')
-                        res.status(201).redirect('/newRoom')
+                        res.status(201).redirect('/myHome')
                     })
             } else {
                 const newRooms = result[0].rooms
-                newRooms.push(req.body)
-                User.findByIdAndUpdate(result[0]._id, { rooms: newRooms })
+                let newRoom = req.body
+                newRoom.plants = 0
+                newRooms.push(newRoom)
+                User.findByIdAndUpdate(result[0]._id, { rooms: newRooms }, {useFindAndModify: false})
                     .then((result) => {
                         console.log('Rooms updated')
-                        res.status(201).redirect('/newRoom')
+                        res.status(201).redirect('/myHome')
                     })
             }
         })
@@ -151,9 +183,15 @@ app.post('/newPlant/:id', (req, res) => {
         .catch(err => console.log(err))
         .then((result) => {
             let resultRoom = ""
+            let newRooms = result[0].rooms
+            let room
+            let newTotal = result[0].totalPlants + 1
             for (let i = 0; i < result[0].rooms.length; i++) {
                 if (result[0].rooms[i]._id == req.params.id) {
-                    resultRoom = result[0].rooms[i].name
+                    room = result[0].rooms[i]
+                    resultRoom = room.name
+                    room.plants++
+                    newRooms[i] = room
                 }
             }
             if (result[0].plants === undefined) {
@@ -167,7 +205,7 @@ app.post('/newPlant/:id', (req, res) => {
                     needsWater: false
                 }
                 newPlants.push(newPlant)
-                User.findByIdAndUpdate(result._id, { plants: newPlants })
+                User.findByIdAndUpdate(result._id, { plants: newPlants, rooms: newRooms, totalPlants: newTotal }, {useFindAndModify: false})
                     .catch(err => console.log(err))
                     .then(() => {
                         console.log('Plants updated')
@@ -184,7 +222,7 @@ app.post('/newPlant/:id', (req, res) => {
                     needsWater: false
                 }
                 newPlants.push(newPlant)
-                User.findByIdAndUpdate(result[0]._id, { plants: newPlants })
+                User.findByIdAndUpdate(result[0]._id, { plants: newPlants, rooms: newRooms, totalPlants: newTotal }, {useFindAndModify: false})
                     .catch(err => console.log(err))
                     .then(() => {
                         console.log('Plants updated')
@@ -208,5 +246,46 @@ app.get('/waterPlant/:id', (req, res) => {
                 .catch(err => console.log(err))
                 .then(console.log("plant watered"))
             res.redirect('/house')
+        })
+})
+
+app.get('/deletePlant/:id', (req, res) => {
+    User.find({ googleId: req.user.googleId })
+        .catch(err => console.log(err))
+        .then((result) => {
+            let newPlants = result[0].plants
+            let newTotal = result[0].totalPlants - 1
+            for (i = 0; i < newPlants.length; i++) {
+                if (newPlants[i]._id == req.params.id) {
+                    newPlants.splice(i, 1)
+                }
+            }
+            User.findOneAndUpdate({ googleId: req.user.googleId }, { plants: newPlants, totalPlants: newTotal }, { useFindAndModify: false })
+                .catch(err => console.log(err))
+                .then(() => {
+                    console.log("Plant deleted")
+                    User.find({ googleId: req.user.googleId })
+                        .catch(err => console.log(err))
+                        .then((result) => {
+                            let newRooms = result[0].rooms
+                            for (i = 0; i < newRooms.length; i++) {
+                                let room = newRooms[i]
+                                let plantCounter = 0
+                                for (j = 0; j < result[0].plants.length; j++) {
+                                    if (result[0].plants[j].room === room.name) {
+                                        plantCounter++
+                                    }
+                                }
+                                room.plants = plantCounter
+                                newRooms[i] = room
+                            }
+                            User.findOneAndUpdate({ googleId: req.user.googleId }, { rooms: newRooms }, { useFindAndModify: false })
+                                .catch(err => console.log(err))
+                                .then(() => {
+                                    console.log("Plant count updated")
+                                    res.status(201).redirect('/myHome')
+                                })
+                        })
+                })
         })
 })
